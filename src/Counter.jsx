090d87f94a7e2e260a578a514cd0443ea6818b82
vaishak1999 <112836742+vaishak1999@users.noreply.github.com>
2023-07-07 19:32:20 +0530

    Todo Completed

---
+++ src/Counter.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Counter({title,...obj}) {
+    
+  return (
+    <div>
+        <h1>{title}: {obj.count}</h1>
+      
+    </div>
+  )
+}
+
+export default Counter
